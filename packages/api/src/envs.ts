6fb76e8c84a6bb0cd9c63822bf0c8441784e67f8
dennisja <dennisjjagwe@gmail.com>
2023-04-04 22:52:41 +0200

    feat(login): start loging implementation

--- packages/api/src/envs.ts
+++ packages/api/src/envs.ts
@@ -2,4 +2,6 @@ const PORT = Number(process.env.API_PORT) || 4000;
 
 const DATABASE_URL = process.env.DATABASE_URL || "";
 
-export { PORT, DATABASE_URL };
+const GOOGLE_AUTH_CLIENT_ID = process.env.GOOGLE_AUTH_CLIENT_ID || "";
+
+export { PORT, DATABASE_URL, GOOGLE_AUTH_CLIENT_ID };
